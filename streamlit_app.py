78ad8174d7743e1f055b45a67e7b6fa7a0179ad6
jolow99 <jolow999@gmail.com>
2023-11-21 09:21:29 +0200

    Added column display

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,14 @@ conn = st.connection("supabase",type=SupabaseConnection)
 
 # Perform query.
 rows = conn.query("*", table="allData", ttl="0").execute()
+latest = rows.data[-1]
 
+# Display current moisture, temperature, and humidity.
+col1, col2, col3 = st.columns(3)
+col1.metric("Moisture", str(latest["moisture"] * 100 ) + "%")
+col2.metric("Temperature", str(latest["temperature"]) + "°C")
+col3.metric("Humidity", str(latest["humidity"]) + "%")
+
+
+# Display raw data
 st.table(rows.data)
